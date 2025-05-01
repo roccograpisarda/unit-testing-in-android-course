96961f437d4131325eab9c4d7ec1c573239ea7f2
Rocco G. Rapisarda <github@roccorapisarda.it>
2025-05-01 08:01:23 +0200

    unit_test_fundamentals: resolved exercise 2

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,37 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringDuplicatorTest {
+
+    StringDuplicator SUT;
+
+
+    @Before
+    public void setup(){
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void test1(){
+       String result =  SUT.duplicate("");
+        Assert.assertThat(result, is(""));
+    }
+
+    @Test
+    public void test2(){
+        String result =  SUT.duplicate("a");
+        Assert.assertThat(result, is("aa"));
+    }
+
+    @Test
+    public void test3(){
+        String result =  SUT.duplicate("aa");
+        Assert.assertThat(result, is("aaaa"));
+    }
 
 }
