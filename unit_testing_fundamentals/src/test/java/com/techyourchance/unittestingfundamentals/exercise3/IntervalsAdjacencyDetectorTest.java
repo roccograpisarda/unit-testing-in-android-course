fc521ea967e0b35623f6e146535118b7e9f1b850
Rocco G. Rapisarda <github@roccorapisarda.it>
2025-05-01 08:01:31 +0200

    unit_test_fundamentals: resolved exercise 3

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
@@ -1,5 +1,44 @@
 package com.techyourchance.unittestingfundamentals.exercise3;
 
+import static org.hamcrest.CoreMatchers.is;
+
+import com.techyourchance.unittestingfundamentals.example3.Interval;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class IntervalsAdjacencyDetectorTest {
+
+    IntervalsAdjacencyDetector SUT;
+
+    @Before
+    public void setup(){
+        SUT = new IntervalsAdjacencyDetector();
+    }
+
+    @Test
+    public void test1(){
+        boolean result = SUT.isAdjacent(new Interval(0, 1), new Interval(1,2));
+        Assert.assertThat(result, is(true));
+    }
+
+    @Test
+    public void test2(){
+        boolean result = SUT.isAdjacent(new Interval(0, 1), new Interval(0,1));
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void test3(){
+        boolean result = SUT.isAdjacent(new Interval(0, 1), new Interval(0,2));
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void test4(){
+        boolean result = SUT.isAdjacent(new Interval(0, 1), new Interval(3,4));
+        Assert.assertThat(result, is(false));
+    }
 
 }
